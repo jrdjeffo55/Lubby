2b51948a3a0d57bedeb3f6a63456fbbc1527983d
Jeff Dugan <jeffdugan5@gmail.com>
2017-10-12 14:29:45 -0700

    Lubby FTW

--- example/models.py
+++ example/models.py
@@ -3,10 +3,16 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class LoggedInUser(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name='logged_in_user')
+
+class Clarify(models.Model):
+    user = models.ForeignKey(User, related_name='user')
+    clear = models.BooleanField()
+    
 
 
